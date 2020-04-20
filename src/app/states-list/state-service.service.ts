74f3141dda7eb9134e853d8a4811569105833ae5
Sreejesh4337 <sreejesh4337@gmail.com>
2020-04-20 19:22:06 +0530

    Added timeout

--- src/app/states-list/state-service.service.ts
+++ src/app/states-list/state-service.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, of, throwError } from 'rxjs';
 
-import { switchMap, catchError, map, take } from 'rxjs/operators';
+import { switchMap, catchError, map, take, timeout } from 'rxjs/operators';
 import { State } from './state.model';
 import { LogService } from '../error-handling/log.service';
 
@@ -38,6 +38,9 @@ export class StateServiceService {
   }
 
   getData(state: string) {
-    return this.http.get<any>(this.argisCode + '=' + state + '&outFields=Match_addr,Addr_type');
+    return this.http.get<any>(this.argisCode + '=' + state + '&outFields=Match_addr,Addr_type')
+    .pipe(
+      timeout(5000)
+    );
   }
 }
